a9b8c2bb0c51b85b6bc007ec894b4e7a5425f003
nitink23 <50839741+nitink23@users.noreply.github.com>
2023-07-03 00:40:10 -0700

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,49 @@
+import os
+import streamlit as st
+from langchain.llms import OpenAI
+from apikey import apikey 
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+
+
+os.environ['OPENAI_API_KEY'] = apikey
+#APP framework
+st.title('🍛CurryGPT🍛')
+prompt = st.text_input('Type in the curry you want to make and see the magic happen and there is a bonus at the bottom of the answer.')
+
+
+#prompt template
+curry_template = PromptTemplate(
+  input_variables = ['curry'],
+  template = 'write how this {curry} is made step by step'
+  
+)
+spicy_template = PromptTemplate(
+  input_variables = ['level'],
+  template = ' which ingredient in {level} controls the spicy level ?'
+)
+
+video_template = PromptTemplate(
+  input_variables = ['video'],
+  template = ' find a youtube video that will help me make {video}'
+)
+
+
+# make this an llm
+lm = OpenAI(temperature = 0.9)
+
+spicy_chain = LLMChain(llm = lm,prompt = spicy_template,verbose = True)
+curry_chain = LLMChain(llm = lm,prompt = curry_template, verbose = True)
+video_chain = LLMChain(llm = lm,prompt = video_template, verbose = True)
+
+# gives out a response
+if prompt:
+  response1 = curry_chain.run(prompt)
+  response2 = spicy_chain.run(prompt)
+  response3 = video_chain.run(prompt)
+  st.write(response1)
+  st.write('The ingredient that controls the spice level: ')
+  st.write(response2)
+  st.write('here is a video to help you make it')
+  st.write(response3)
