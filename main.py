aebd956a112fc12320feaa54c0071d6d91f45151
nitink23 <50839741+nitink23@users.noreply.github.com>
2023-07-04 12:26:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
+import sys
+print(sys.path)
+
 import streamlit as st
 from langchain.llms import OpenAI
-from apikey import apikey 
+from apikey import apikey
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
