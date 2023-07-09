caaa4a33497f84bf118e9447abf9f8fcff55c043
nitink23 <50839741+nitink23@users.noreply.github.com>
2023-07-09 12:43:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import os
 import streamlit as st
 from langchain.llms import OpenAI
-from apikey import apikey 
+from apikey import apikey
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
+def print_text_animated(text):
+  for char in text:
+    st.write(char, end = '', flush = True)
+    time.sleep(0.02)
 
 
 os.environ['OPENAI_API_KEY'] = apikey
@@ -17,14 +21,14 @@ prompt = st.text_input('Type in the curry you want to make and see the magic hap
 ingredient_template = PromptTemplate(
   input_variables = ['ingredient'],
   template = 'list out in bullet points with a line in between the ingredients needed to make {ingredient}'
-  
+
 )
 
 
 curry_template = PromptTemplate(
   input_variables = ['curry'],
   template = 'write how this {curry} is made step by step'
-  
+
 )
 spicy_template = PromptTemplate(
   input_variables = ['level'],
@@ -38,22 +42,30 @@ video_template = PromptTemplate(
 
 
 # make this an llm
-lm = OpenAI(temperature = 0.9)
+lm = OpenAI(temperature = 0.9,max_tokens=400)
 
 ingredient_chain = LLMChain(llm = lm, prompt = ingredient_template ,verbose = True)
 spicy_chain = LLMChain(llm = lm,prompt = spicy_template,verbose = True)
 curry_chain = LLMChain(llm = lm,prompt = curry_template, verbose = True)
 video_chain = LLMChain(llm = lm,prompt = video_template, verbose = True)
 
+
 # gives out a response
 if prompt:
-  response1 = curry_chain.run(prompt)
-  response2 = spicy_chain.run(prompt)
   
+  
+
   response4 = ingredient_chain.run(prompt)
+  
+  
+  
   st.write('The ingredients to make this dish are:-')
   st.write(response4)
+ 
+  response1 = curry_chain.run(prompt)
   st.write('The steps of preparation')
   st.write(response1)
+  
+  response2 = spicy_chain.run(prompt)
   st.write('The ingredient that controls the spice level: ')
   st.write(response2)
